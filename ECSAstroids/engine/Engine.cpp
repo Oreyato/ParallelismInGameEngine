fe162f9d608e60ca9a2e3a21ad44ffe1159ac33a
oreyato <oreyato.pro@gmail.com>
2023-04-24 12:37:07 +0200

    feat: engine preparations

---
+++ ECSAstroids/engine/Engine.cpp
@@ -0,0 +1,34 @@
+#include <algorithm>
+
+#include "raylib.h"
+
+#include "Engine.hpp"
+#include "Window.hpp"
+#include "IGame.hpp"
+
+
+void Engine::Start(i32 windowWidth, i32 windowHeight, const str& gameNameP,
+	unique_ptr<IGame>&& gameP) noexcept
+{
+	gameName = gameNameP;
+	game = std::move(gameP);
+
+	Window window{ windowWidth, windowHeight, gameName };
+	SetTargetFPS(60);
+
+	game->Load();
+
+	while (!Window::ShouldClose())
+	{
+		f32 dt = std::min(GetFrameTime(), 0.0166f);
+		game->Update(dt);
+
+		render::BeginDraw();
+		render::ClearScreen();
+		game->Draw();
+		DrawFPS(50, 50);
+		render::EndDraw();
+	}
+
+	game->Unload();
+}
