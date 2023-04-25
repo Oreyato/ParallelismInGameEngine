66dee63f21b03cf10fc1868a46c079550a0df24e
oreyato <oreyato.pro@gmail.com>
2023-04-25 14:24:29 +0200

    feat: load textures and create the ship entity

--- ECSAstroids/game/SceneGame.cpp
+++ ECSAstroids/game/SceneGame.cpp
@@ -9,12 +9,43 @@ SceneGame::SceneGame(shared_ptr<ECSManager> ecsRef,	Game& game):
 }
 
 void SceneGame::Load() {
+	i32 sceneId = ToSceneId(SceneName::SceneGame);
+
+	//v Load textures ================================================
+	AssetsManager::LoadTexture("Ship", "../resources/Ship.png", sceneId);
+	AssetsManager::LoadTexture("ShipWithThrust", "../resources/ShipWithThrust.png", sceneId);
+	AssetsManager::LoadTexture("Laser", "../resources/Laser.png", sceneId);
+
+	AssetsManager::LoadTexture("Astroid", "../resources/Astroid.png", sceneId);
+
+	AssetsManager::LoadTexture("Farback01", "../resources/Farback01.png", sceneId);
+	AssetsManager::LoadTexture("Stars", "../resources/Stars.png", sceneId);
+
+	//^ Load textures ================================================
+	//v Create entities ==============================================
+	// -- SHIP --
+	u64 ship = ecs->CreateEntity();
+	Transform2D shipTransform = ecs->CreateTransform2DComponent(ship);
+	Sprite shipSprite = ecs->CreateSpriteComponent(ship, "Ship");
+	Rigidbody2D shipRb = ecs->CreateRigidbody2DComponent(ship, Vector2{ 0, 0 }, shipSprite.srcRect);
+
+	// -- ASTROIDS --
+
+	// -- BACKGROUND --
+	// Far back
+
+	// Stars
+
+	//^ Create entities ==============================================
 }
 
 void SceneGame::Update(f32 dt) {
+
 }
 
 void SceneGame::Draw() {
+	ecs->PrepareDraw();
+	ecs->DrawScene();
 }
 
 void SceneGame::Unload() {
