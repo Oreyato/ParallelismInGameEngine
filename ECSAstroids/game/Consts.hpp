4062c4c3e38939f6d266feedd5a6ea99858fdd11
oreyato <oreyato.pro@gmail.com>
2023-04-25 16:41:23 +0200

    ref: static const window dimensions

--- ECSAstroids/game/Consts.hpp
+++ ECSAstroids/game/Consts.hpp
@@ -1,6 +1,6 @@
 #include "../engine/Defines.hpp"
 
 namespace Consts {
-	static i32 WINDOW_WIDTH{ 1280 };
-	static i32 WINDOW_HEIGHT{ 720 };
+	static const i32 WINDOW_WIDTH{ 1280 };
+	static const i32 WINDOW_HEIGHT{ 720 };
 }
