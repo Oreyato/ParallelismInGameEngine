ee00ca57a9915f0e17101e770e5c1e4831f2231a
oreyato <oreyato.pro@gmail.com>
2023-04-25 15:44:30 +0200

    feat: randomly spawn astroids

--- ECSAstroids/game/SceneGame.cpp
+++ ECSAstroids/game/SceneGame.cpp
@@ -1,6 +1,12 @@
 #include "SceneGame.hpp"
 #include "../engine/AssetsManager.hpp"
 
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+
+
+
 
 SceneGame::SceneGame(shared_ptr<ECSManager> ecsRef,	Game& game): 
 	ecs{ std::move(ecsRef) }, 
@@ -9,6 +15,7 @@ SceneGame::SceneGame(shared_ptr<ECSManager> ecsRef,	Game& game):
 }
 
 void SceneGame::Load() {
+	srand((unsigned)time(0));
 	i32 sceneId = ToSceneId(SceneName::SceneGame);
 
 	//v Load textures ================================================
@@ -30,8 +37,34 @@ void SceneGame::Load() {
 
 	Sprite& shipSprite = ecs->CreateSpriteComponent(ship, "Ship");
 	Rigidbody2D& shipRb = ecs->CreateRigidbody2DComponent(ship, Vector2{ 0, 0 }, shipSprite.srcRect);
+	shipRb.velocity = { 40, 10 };
 	  
 	// -- ASTROIDS --
+	i16 maxAstroidsCount{ 10 };
+	i16 maxXPos = 1280 * 0.9f;
+	i16 maxYPos = 720 * 0.9f;
+	i16 maxVelocity = 50;
+	i16 minVelocity = -50;
+
+	for (i16 i = 0; i < maxAstroidsCount; i++) {
+		// Random position
+		int randXPos = (rand() % maxXPos);
+		int randYPos = (rand() % maxYPos);
+		Vector2 randPos = { randXPos, randYPos };
+
+		// Random velocity
+		int randXVel = (rand() % (maxVelocity - minVelocity)) + minVelocity;
+		int randYVel = (rand() % (maxVelocity - minVelocity)) + minVelocity;
+		Vector2 randVel = { randXVel, randYVel };
+		
+		u64 astroid = ecs->CreateEntity();
+		Transform2D& astroidTransform = ecs->CreateTransform2DComponent(astroid);
+		astroidTransform.pos = randPos;
+
+		Sprite& astroidSprite = ecs->CreateSpriteComponent(astroid, "Astroid");
+		Rigidbody2D& astroidRb = ecs->CreateRigidbody2DComponent(astroid, Vector2{ 0, 0 }, astroidSprite.srcRect);
+		astroidRb.velocity = randVel;
+	}
 
 	// -- BACKGROUND --
 	// Far back
@@ -42,7 +75,7 @@ void SceneGame::Load() {
 }
 
 void SceneGame::Update(f32 dt) {
-	
+	ecs->UpdateScene(dt);
 }
 
 void SceneGame::Draw() {
